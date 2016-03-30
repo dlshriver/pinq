b6f3bbdcc0f970f6808645c6a1660bd2024b58ed
David Shriver <dlshriver@cox.net>
2016-03-29 21:39:21 -0500

    add documentation to api and queryable

--- pylinq/api.py
+++ pylinq/api.py
@@ -9,9 +9,24 @@ This module implements the pylinq api.
 """
 
 from collections import Iterable
-from .queryable import _Queryable
+from .queryable import Queryable
 
-def as_queryable(iterable, cacheable=False):
+
+def as_queryable(iterable):
+    """Constructs a queryable object using `iterable` as the base data.
+
+    :param iterable: iterable object to make queryable.
+    :type iterable: Iterable
+    :return: a queryable object with the specified iterable as the underlying data
+    :rtype: :class:`Queryable <Queryable>` object
+    :raise TypeError: if iterable is not an Iterable
+
+    Usage::
+
+      >>> import pylinq
+      >>> queryable = pylinq.as_queryable(range(100))
+      range(0, 100)
+    """
     if not isinstance(iterable, Iterable):
         raise TypeError("Object must be iterable.")
-    return _Queryable(iterable)
+    return Queryable(iterable)
