f9b5a257f3f94d10fb1c437c27337ed90d1039cc
David Shriver <dlshriver@cox.net>
2016-04-06 11:07:32 -0500

    add history

---
+++ HISTORY.rst
@@ -0,0 +1,21 @@
+.. :changelog:
+
+RELEASE HISTORY
+---------------
+
+0.1.1 (???)
++++++++++++
+
+* Added documentation
+    - History
+    - Static docs (not done)
+* Refactor
+    - Add more tests and split old tests into distinct modules
+    - Add `OrderedQueryable`
+    - Allow multiple arguments in passed lambda functions 
+* Bugfixes
+
+0.1.0 (31-03-2016)
+++++++++++++++++++
+
+* Initial release
--- setup.py
+++ setup.py
@@ -4,15 +4,35 @@ pylinq setup script.
 
 from distutils.core import setup
 
+with open("README.rst", 'r') as f:
+    readme = f.read()
+with open("HISTORY.rst", 'r') as f:
+    history = f.read()
+
 setup(
     name='pinq',
-    version='0.2.0',
+    version='0.1.1',
     description='LINQ for python.',
+    long_description="%s\n\n%s" % (readme, history),
     license='MIT',
     author='David Shriver',
     author_email='david.shriver@outlook.com',
     url='https://github.com/dlshriver/pinq',
     packages=[
         'pinq',
-    ]
+    ],
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    )
 )
