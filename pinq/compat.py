9f3e823926f966321dd95833cce25f12108774cb
David Shriver <dlshriver@cox.net>
2016-04-06 18:23:55 -0500

    add tests for compatibility layer

--- pinq/compat.py
+++ pinq/compat.py
@@ -12,9 +12,10 @@ try:
     from functools import lru_cache
 except ImportError:
     def lru_cache(maxsize=128, typed=False):
+        """A default decorator if lru_cache does not exist."""
         return lambda f: f
 
 try:
     from itertools import zip_longest
-except:
+except ImportError:
     from itertools import izip_longest as zip_longest
